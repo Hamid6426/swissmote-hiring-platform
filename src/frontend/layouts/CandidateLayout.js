d455365ecb938bee95ecf655f50405ff0bd81825
hamid6426 <mianhamid6426@gmail.com>
2024-11-24 22:31:00 +0500

    some addition

--- src/frontend/layouts/CandidateLayout.js
+++ src/frontend/layouts/CandidateLayout.js
@@ -5,7 +5,7 @@ export default function CandidateLayout({ children }) {
   return (
     <div className="bg-white dark:bg-neutral-800 dark:text-white w-full min-h-screen flex flex-col px-6">
       <CandidateNavbar />
-      <div className="w-full h-fit flex flex-row -z-50">
+      <div className="w-full h-auto flex flex-row -z-50">
         <CandidateSidebar />
         <section className="w-full min-h-svh rounded-xl bg-green-200 dark:bg-neutral-900 dark:text-white p-6 mb-6">
           {children}
